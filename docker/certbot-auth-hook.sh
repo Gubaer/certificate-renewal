b5b2cf3e1414bb0953d3077e6914fc0d3e10dc57
Karl Guggisberg <karl.guggisberg@kacon.ch>
2019-03-02 14:41:31 +0100

    remove former implementation version

--- docker/certbot-auth-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "CERTBOT_VALIDATION: $CERTBOT_VALIDATION"
-echo "CERTBOT_TOKEN: $CERTBOT_TOKEN"
-
-mkdir -p /certificate-renewal/challenges
-echo $CERTBOT_VALIDATION > /certificate-renewal/challenges/$CERTBOT_TOKEN
-aws s3 cp /certificate-renewal/challenges/$CERTBOT_TOKEN s3://www.kacon.ch/.well-known/acme-challenge/$CERTBOT_TOKEN
-sleep 5
--- docker/certbot-cleanup-hook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# remove the certbot challenge 
-aws s3 rm s3://www.kacon.ch/.well-known/acme-challenge/$CERTBOT_TOKEN
